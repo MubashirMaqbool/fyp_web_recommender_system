52eeb7e8619428327fe5561c10cf118920f3c7cb
unknown <MuhammadUmaira98@gmail.com>
2021-03-05 17:16:54 +0500

    made login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,4 +26,18 @@ class UserController extends Controller
 
 
     }
+    public function login(Request $request){
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+
+    }
 }
